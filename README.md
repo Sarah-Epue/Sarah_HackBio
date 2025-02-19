087c1503fd049b98adbe91a7a9846f0a4c7664b4
Sarah-Epue <sarahaleonogwe@gmail.com>
2025-02-19 12:37:38 +0000

    stage 0 updated task

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# Sarah_HackBio
+ Sarah_HackBio
+ assign intended display text to a variable
+ use curved brackets to create a tuple
+ use double quotes to enclose strings to form tags
+ enclose: in a double quote so it is read as a tag and not a command
+ add spaces where necessary
+ assign each intended display text one line per variable
+ write a print command for every variable, one in a separate line
---
+++ stage_0.py
@@ -0,0 +1,23 @@
+#assign intended display text to a variable
+#use curved brackets to create a tuple
+#use double quotes to enclose strings to form tags
+#enclose: in a double quote so it is read as a tag and not a command
+#add spaces whw=ere necessary
+a = ("Sarah's Personal Information:")
+b = ("Name"": " "Sarah Epue Aleonogwe")
+c = ("Slack Username"": " "Sarah")
+d = ("Email"": " "sarahaleonogwe@gmail.com")
+e = ("Hobbies"": " "Watching, animae, Reading, Eating")
+f = ("Country"": " "Nigeria")
+g = ("Discipline"": " "Medical Physiology")
+h = ("Preferred Programming Language"": " "Python")
+#assign each intended display text one line per variable
+#write a print command for every variable, one in a separate line
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
